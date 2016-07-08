91f45300ade6dd8c1dcec949f48508502fec9f9d
CrowdHailer <peterhsaxton@gmail.com>
2016-07-08 18:24:43 +0100

    thorough rename

--- test/test.js
+++ test/test.js
@@ -3,128 +3,128 @@ var assert = require('chai').assert;
 var Server = {
   start: function () {
     // Property test reduction of history should always return state
-    var state
-    var history = []
+    var state;
+    var history = [];
     return {
       getHistory: function () {
         // optionally pass in as of to start history searcg
-        return history
+        return history;
       },
-      submitPatch: function (patch) {
-        history.push(patch)
-        return {ok: true}
+      saveChangeset: function (patch) {
+        history.push(patch);
+        return {ok: true};
       }
-    }
+    };
   }
-}
+};
 
 var Client =  {
   start: function (server) {
     // record latest server timestamp
-    state = {}
-    var changeSet = {}
+    state = {};
+    var changeset = {}; // Patch, Revision
     return {
       setValue: function (key, value) {
-        changeSet[key] = {from: state[key], to: value}
+        changeset[key] = {from: state[key], to: value};
       },
       getLocalState: function () {
-        var patch = {}
-        Object.keys(changeSet).forEach(function(key){
-          if (state[key] === changeSet[key].from) {
-            patch[key] = changeSet[key].to
+        var patch = {};
+        Object.keys(changeset).forEach(function(key){
+          if (state[key] === changeset[key].from) {
+            patch[key] = changeset[key].to;
           } else {
-            patch[key] = {remote: state[key], local: changeSet[key].to}
+            patch[key] = {remote: state[key], local: changeset[key].to};
           }
-        })
-        return Object.assign({}, state, patch)
+        });
+        return Object.assign({}, state, patch);
       },
-      getLocalChanges: function () {
-        return changeSet
+      getChangeset: function () {
+        return changeset;
       },
-      saveChanges: function () {
-        response = server.submitPatch(changeSet)
+      saveChangeset: function () {
+        response = server.saveChangeset(changeset);
         if (response.ok) {
-          changeSet = {}
+          changeset = {};
         }
       },
       checkServer: function () {
-        var patch = {}
-        server.getHistory().forEach(function (changeSet) {
-          Object.keys(changeSet).forEach(function(key){
-            patch[key] = changeSet[key].to
-          })
-        })
-        state = Object.assign({}, state, patch)
+        var patch = {};
+        server.getHistory().forEach(function (changeset) {
+          Object.keys(changeset).forEach(function(key){
+            patch[key] = changeset[key].to;
+          });
+        });
+        state = Object.assign({}, state, patch);
       }
-    }
+    };
   }
-}
+};
 
 describe('Central Server', function () {
   describe('viewing', function () {
     it('should start with an empty history', function () {
-      var server = Server.start({})
-      assert.equal(0, server.getHistory().length)
+      var server = Server.start({});
+      assert.equal(0, server.getHistory().length);
     });
-  })
+  });
 });
 describe('Client', function () {
   describe('editing a value', function () {
     // localchanges rename to change set
     it('should start with an empty set of changes', function () {
-      var client = Client.start()
-      assert.equal(Object.keys(client.getLocalChanges()).length, 0)
+      var client = Client.start();
+      assert.equal(Object.keys(client.getChangeset()).length, 0);
     });
     it('should be added to the changeset', function () {
-      var client = Client.start()
-      client.setValue('foo', 'something')
-      assert.equal(Object.keys(client.getLocalChanges()).length, 1)
+      var client = Client.start();
+      client.setValue('foo', 'something');
+      assert.equal(Object.keys(client.getChangeset()).length, 1);
     });
     it('should change the value locally', function () {
-      var client = Client.start()
-      client.setValue('foo', 'something')
-      assert.equal(client.getLocalState().foo, 'something')
+      var client = Client.start();
+      client.setValue('foo', 'something');
+      assert.equal(client.getLocalState().foo, 'something');
     });
     it('should modify the changeset for a second change to a key', function () {
-      var client = Client.start()
-      client.setValue('foo', 'something')
-      client.setValue('foo', 'else')
-      assert.equal(client.getLocalState().foo, 'else')
+      var client = Client.start();
+      client.setValue('foo', 'something');
+      client.setValue('foo', 'else');
+      assert.equal(client.getLocalState().foo, 'else');
     });
     // Should delete change if changes to some server value
-  })
+  });
   describe('saving changes', function () {
     it('should clear changeset is server accepts changes', function () {
-      var client = Client.start({submitPatch: function () { return {ok: true}}})
-      client.setValue('foo', 'bar')
-      client.saveChanges()
-      assert.deepEqual(client.getLocalChanges(), {})
-    })
+      var client = Client.start({saveChangeset: function () { return {ok: true}; }});
+      client.setValue('foo', 'bar');
+      client.saveChangeset();
+      assert.deepEqual(client.getChangeset(), {});
+    });
     // Should not try to save if conflicts remain
-  })
+  });
 });
 describe('Client Server interaction', function () {
   it('should follow this story of two halves', function () {
-    var server = Server.start({})
-    var clientA = Client.start(server)
+    var server = Server.start({});
+    var clientA = Client.start(server);
 
-    clientA.setValue('foo', 'first')
-    assert.equal(clientA.getLocalState().foo, 'first')
-    assert.deepEqual(clientA.getLocalChanges().foo, {from: undefined, to: 'first'})
+    clientA.setValue('foo', 'first');
+    assert.equal(clientA.getLocalState().foo, 'first');
+    assert.deepEqual(clientA.getChangeset().foo, {from: undefined, to: 'first'});
 
-    clientA.saveChanges()
+    clientA.saveChangeset();
 
-    var clientB = Client.start(server)
-    clientB.checkServer()
-    assert.equal(clientB.getLocalState().foo, 'first')
+    var clientB = Client.start(server);
+    clientB.checkServer();
+    assert.equal(clientB.getLocalState().foo, 'first');
 
-    clientB.setValue('foo', 'second')
-    clientB.saveChanges()
+    clientB.setValue('foo', 'second');
+    clientB.saveChangeset();
 
-    clientA.setValue('foo', 'other')
-    clientA.checkServer()
-    assert.deepEqual(clientA.getLocalState().foo, {remote: 'second', local: 'other'})
-    clientA.setValue('foo', 'altogether different')
-    assert.deepEqual(clientA.getLocalState().foo, 'altogether different')
-  })
+    clientA.setValue('foo', 'other');
+    clientA.checkServer();
+    assert.deepEqual(clientA.getLocalState().foo, {remote: 'second', local: 'other'});
+    clientA.setValue('foo', 'altogether different');
+    assert.deepEqual(clientA.getLocalState().foo, 'altogether different');
+  });
 });
